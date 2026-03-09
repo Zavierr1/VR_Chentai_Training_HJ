74c6d3a8d43b2e262f996718fe72d505178e83e0
Muhammad Fadel Jafier <muhammad.jafier@student.president.ac.id>
2026-03-09 11:35:36 +0700

    update tablejadispawner.cs

--- Assets/Script Chentai/TabletJadiSpawner.cs
+++ Assets/Script Chentai/TabletJadiSpawner.cs
@@ -2,51 +2,40 @@ using UnityEngine;
 
 public class TabletJadiSpawner : MonoBehaviour
 {
-   [Header("Pengaturan Spawn")]
-    [Tooltip("Masukkan prefab tablet yang sudah jadi (depan belakang) ke sini.")]
+    [Header("Pengaturan Spawn")]
     public GameObject tabletPrefab;
-
-    [Tooltip("Titik lokasi di mana tablet akan muncul (buat Empty GameObject di posisi cetakan).")]
     public Transform spawnPoint;
-
     public float tabletLifetime = 3f;
 
+    [Header("Koneksi ke Ejector")]
+    [Tooltip("Tarik objek yang punya script EjectorController ke kotak ini")]
+    public EjectorController ejectorMesin; // <--- BARIS BARU
+
     [Header("Efek Tambahan (Opsional)")]
-    [Tooltip("Masukkan efek partikel debu/percikan jika ada.")]
     public ParticleSystem pressEffect; 
-    
-    [Tooltip("Masukkan suara mesin press di sini.")]
     public AudioClip pressSound;
     private AudioSource audioSource;
 
     void Start()
     {
-        // Mengambil komponen AudioSource jika kamu ingin menambahkan suara
         audioSource = GetComponent<AudioSource>();
     }
 
-    // Fungsi ini yang akan memunculkan tablet
     public void SpawnTablet()
     {
         if (tabletPrefab != null && spawnPoint != null)
         {
-            // Melakukan spawn prefab tablet tepat di posisi dan rotasi titik spawnPoint
             GameObject newTablet = Instantiate(tabletPrefab, spawnPoint.position, spawnPoint.rotation);
-
             Destroy(newTablet, tabletLifetime);
             
-
-            // Memainkan efek partikel (opsional)
-            if (pressEffect != null)
+            // >>> TAMBAHAN BARU: Suruh ejector melempar dadu probabilitas!
+            if (ejectorMesin != null)
             {
-                pressEffect.Play();
+                ejectorMesin.CekEject();
             }
 
-            // Memainkan suara (opsional)
-            if (pressSound != null && audioSource != null)
-            {
-                audioSource.PlayOneShot(pressSound);
-            }
+            if (pressEffect != null) pressEffect.Play();
+            if (pressSound != null && audioSource != null) audioSource.PlayOneShot(pressSound);
         }
         else
         {
